c3945f7e85b32dce24a35985b701672032960ba4
Willie Wheeler <wwheeler@expedia.com>
2016-01-26 12:15:09 -0800

    WIP and potential handoff to Zaal

--- lib/seiso/import_chef.rb
+++ lib/seiso/import_chef.rb
@@ -1,92 +1,91 @@
-require "chef/node"
-require "chef/rest"
-require "seiso/connector"
-require_relative "import_chef/chef_machine_mapper"
+require 'chef/node'
+require 'chef/rest'
+require 'hyper_resource'
+require 'require_all'
+require_rel './import_chef'
 
 # Seiso namespace module
 module Seiso
-  
+
   # Imports Chef nodes into Seiso as Seiso machines.
   #
   # Author:: Willie Wheeler (mailto:wwheeler@expedia.com)
   # Copyright:: Copyright (c) 2014-2015 Expedia, Inc.
   # License:: Apache 2.0
   class ImportChef
-    
+
     # Batch size for putting nodes into Seiso.
     PAGE_SIZE = 20
-    
+
     def initialize(chef_settings, seiso_settings)
       @chef = Chef::REST.new(
         "#{chef_settings['base_uri']}",
         "#{chef_settings['client_name']}",
         "#{chef_settings['signing_key']}")
-      @seiso = Seiso::Connector.new seiso_settings
       @mapper = Seiso::ImportChef::ChefMachineMapper.new
+      @log = Util::Logger.new "ImportChef"
     end
 
-    def seiso
-      @seiso
-    end
-    
     # Imports all nodes from the Chef server into Seiso.
     def import_all
       nodes = @chef.get_rest "/nodes"
       node_names = nodes.keys.sort!
-      
+      @log.info node_names
+
       slices = node_names.each_slice(PAGE_SIZE).to_a
       node_count = 0
       problem_nodes = []
       slices.each_with_index do |slice, page_index|
-        puts "Building page #{page_index + 1} (total_nodes=#{node_names.length}, page_size=#{PAGE_SIZE})"
+        @log.info "Building page #{page_index + 1} (total_nodes=#{node_names.length}, page_size=#{PAGE_SIZE})"
         page = []
         slice.each do |name|
           # name is case-sensitive in Chef, so we can't just downcase it
           node_count += 1
-          puts "Getting node #{node_count} of #{node_names.length}: #{name}"
+          @log.info "Getting node #{node_count} of #{node_names.length}: #{name}"
           begin
             node = @chef.get_rest "/nodes/#{name}"
             node_automatic = node.automatic
-            
+
             # Have observed this. Need to decide whether to handle default/override/etc.
             if node_automatic.empty?
-              puts "WARNING: Node #{name} has no automatic attributes"
+              @log.warn "Node #{name} has no automatic attributes"
               problem_nodes << {
-                "name" => name,
-                "reason" => "Node has no automatic attributes."
+                'name' => name,
+                'reason' => "Node has no automatic attributes."
               }
               next
             end
-            
+
             machine = @mapper.map_one(node_automatic)
-            
+
             # FIXME Just setting this here for now since I'm not sure which node field the name corresponds to.
             # It's not exactly the FQDN (the case is different).
             machine['name'] = name
             page << machine
           rescue Net::HTTPServerException
-            puts "ERROR: Couldn't get #{name}"
+            @log.error "Couldn't get #{name}"
             problem_nodes << {
-              "name" => name,
-              "reason" => "Couldn't get node from Chef server even though Chef server provided the name."
+              'name' => name,
+              'reason' => "Couldn't get node from Chef server even though Chef server provided the name."
             }
           end
         end
-        
-        # FIXME This is not detecting some HTTP 500s that are happening. When this happens, the whole page fails, the server
-        # returns an HTTP 500 (I think, anyway--need to confirm), and this call doesn't catch the error. Somehow we need to
-        # surface these as problem nodes too, maybe indicating that the server didn't like them, as opposed to not being able to
-        # read them from Chef server.
-        puts "Flushing nodes to Seiso"
-        seiso.post_items('machines', page)
+
+        # FIXME This is not detecting some HTTP 500s that are happening. When this happens, the whole page fails, the
+        # server returns an HTTP 500 (I think, anyway--need to confirm), and this call doesn't catch the error. Somehow
+        # we need to surface these as problem nodes too, maybe indicating that the server didn't like them, as opposed
+        # to not being able to read them from Chef server.
+        @log.info "Flushing nodes to Seiso"
+#        seiso.post_items('machines', page)
       end
-      
+
       if problem_nodes.empty?
-        puts "Successfully imported all machines"
+        @log.success "Successfully imported all machines"
       else
-        puts "Had problem importing #{problem_nodes.length} nodes:"
-        puts problem_nodes
+        @log.warn "Had problem importing #{problem_nodes.length} nodes:"
+        @log.warn problem_nodes
       end
-    end
-  end
-end
+
+    end # method import_all
+  end # class ImportChef
+end # module Seiso
--- lib/seiso/import_chef/chef_machine_mapper.rb
+++ lib/seiso/import_chef/chef_machine_mapper.rb
@@ -1,24 +1,23 @@
+require 'awesome_print'
+
 module Seiso
   class ImportChef
-    
+
     # Maps Chef nodes to Seiso machines.
-    # 
+    #
     # Author:: Willie Wheeler (mailto:wwheeler@expedia.com)
-    # Copyright:: Copyright (c) 2014-2015
+    # Copyright:: Copyright (c) 2014-2016
     # License:: Apache 2.0
     class ChefMachineMapper
-    
+
       # Maps a list of Chef nodes to a list of Seiso machines.
       def map_all(chef_nodes)
         return nil if chef_nodes.nil?
-        seiso_machines = []
-        chef_nodes.each do |n|
-          seiso_machines << map_one(n)
-        end
-        seiso_machines
+        chef_nodes.map { |n| map_one n }
       end
-      
+
       def map_one(chef_node)
+        ap chef_node
         return nil if chef_node.nil?
         {
           # FIXME Not sure which field Chef uses as the key here, so we'll just set that externally for now.
@@ -26,25 +25,26 @@ module Seiso
           "fqdn" => cleanup(chef_node["fqdn"]),
           "hostname" => cleanup(chef_node["hostname"]),
           "domain" => cleanup(chef_node["domain"]),
-          
+
           # TODO For the nodes so far, this is a single IP address. Not sure whether Chef supports multiple IPv4 addresses,
           # or what it returns in that case. Need to look into it.
           "ipAddress" => cleanup(chef_node["ipaddress"]),
-          
+
           # FIXME Commenting this out for now, as it generates arrays where Seiso expects a single value. Not sure why
           # Chef uses a single value for IPv4 but an array for IPv6.
           #      "ip6Address" => cleanup(chef_node["ip6address"]),
-          
+
           "macAddress" => cleanup(chef_node["macaddress"]),
           "platform" => cleanup(chef_node["platform"]),
           "platformVersion" => cleanup(chef_node["platform_version"]),
           "os" => cleanup(chef_node["os"]),
-          "osVersion" => cleanup(chef_node["os_version"])
+          "osVersion" => cleanup(chef_node["os_version"]),
+          "serialNumber" => cleanup(chef_node["serial_number"])
         }
       end
-      
+
       private
-      
+
       def cleanup(s)
         s.nil? ? nil : s.strip.downcase
       end
